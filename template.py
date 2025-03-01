cfad1334b80872a1ecab848f04469dafd2329112
Swapnil Salunke <swapnil.salunkhe@matoshri.edu.in>
2025-03-01 23:49:31 +0530

    data_ingestion file

--- template.py
+++ template.py
@@ -11,7 +11,7 @@ list_of_files = [
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/utils/__init__.py",
-    f"src/{project_name}/utils/common,py",
+    f"src/{project_name}/utils/common.py",
     f"src/{project_name}/logging//__init__.py",
     f"src/{project_name}/config/__init__.py",
     f"src/{project_name}/config/configuration.py",
@@ -37,9 +37,10 @@ for filepath in list_of_files:
         os.makedirs(filedir,exist_ok=True)
         logging.info(f"Creating directory: {filedir} for the file {filename}")
         
-    if(not os.path.exists(filepath)) or (os.path.getsie(filepath)==0):
+    if(not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath,"w") as f:
-            logging.info(f"Creating file: {filepath}")
+            pass
+            logging.info(f"Creating empty file: {filepath}")
             
             
             
